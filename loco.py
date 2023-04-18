067e493e473c0dde55ef7b0b50bb2575172c6e67
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2023-04-19 00:37:58 +0900

    Update loco.py

--- loco.py
+++ loco.py
@@ -7,7 +7,7 @@ from google.cloud import translate
 
 # API Keys
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "assets/b7f9.json"
-openai.api_key = "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"
+openai.api_key = "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"
 google_trans_id = "delta-coast-382412"
 
 
